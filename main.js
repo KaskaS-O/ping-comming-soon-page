0928403afb13672367b8a625b6b428bb58dab183
KaskaS-O <kaska.salaban@gmail.com>
2021-10-07 12:18:53 +0200

    Fix problem with multiple .subscription_msg visibility

--- main.js
+++ main.js
@@ -4,16 +4,12 @@ const form = document.querySelector('.subscription__form');
 
 const formMsg = document.querySelector('.subscription__msg');
 
-let error = null;
-
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     if (input.value.length < 5 || !input.value.includes('@') || !input.value.includes('.')) {
-        error = document.createElement('p');
         formMsg.style = 'display:block';
-        formMsg.appendChild(error);
-        error.innerText = 'Please provide a valid email address';
+        formMsg.innerText = 'Please provide a valid email address';
         input.classList.add('error');
         
     } else {
