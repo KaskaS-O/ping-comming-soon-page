a340d6b51de34eaf73099a6cb216d13207391789
KaskaS-O <kaska.salaban@gmail.com>
2021-10-08 11:34:04 +0200

    Fix email validation, update README.md and .gitignore

--- main.js
+++ main.js
@@ -7,12 +7,15 @@ const formMsg = document.querySelector('.subscription__msg');
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    if (input.value.length < 5 || !input.value.includes('@') || !input.value.includes('.')) {
+    if (!input.value.length) {
+        formMsg.style = 'display:block';
+        formMsg.innerText = 'Whoops! It looks like you forgot to add your email';
+        input.classList.add('error');
+    } else if (input.value.length < 7 || !input.value.includes('@') || !input.value.includes('.')) {
         formMsg.style = 'display:block';
         formMsg.innerText = 'Please provide a valid email address';
         input.classList.add('error');
-        
-    } else {
+    }  else {
         input.value = '';
         formMsg.style ='display:none';
         input.classList.remove('error');
